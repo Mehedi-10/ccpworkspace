a28b283e575ccc777ba5576270a1f9d3e01b73d4
Mehedi Hasan Arafat <mehedihasanarafat10@gmail.com>
2022-09-03 19:37:13 +0600

    template v22.1

--- cmake-build-debug/main.cpp
+++ cmake-build-debug/main.cpp
@@ -26,9 +26,10 @@ int main() {
             cin >> cost[i];
         }
 
-        vector<vector<vector<long long>>> mat(n + 1, vector<vector<long long>>(m + 1,vector<long long>(9,mxxx)));
+        vector<vector<vector<long long>>> mat(n + 1, vector<vector<long long>>(m + 1, vector<long long>(9, mxxx)));
         vector<vector<vector<pair<pair<int, int>, long long >>>> tunnel(n + 1,
-                                                                 vector<vector<pair<pair<int, int>, long long >>>(m + 1));
+                                                                        vector<vector<pair<pair<int, int>, long long >>>(
+                                                                                m + 1));
         for (int i = 0; i < p; ++i) {
             cin >> x1 >> y1 >> x2 >> y2 >> w;
             tunnel[x1][y1].push_back({{x2, y2}, w});
@@ -41,16 +42,15 @@ int main() {
             }
         }
 
-        if (mat[s2][d2][0] == -1)
-        {
+        if (mat[s2][d2][0] == -1) {
             case(++te, ' ');
             cout << -1 << endl;
             continue;
-
         }
 
         queue<pair<int, pair<int, int>>> qq;
         qq.push({0, {s1, d1}});
+
         for (int i = 0; i < 9; ++i) {
             mat[s1][d1][i] = 0;
         }
@@ -60,41 +60,46 @@ int main() {
 
             auto p = qq.front();
             qq.pop();
+
             for (int i = 1; i <= 8; ++i) {
                 int xx = p.second.first + fx[i];
                 int yy = p.second.second + fy[i];
                 long long costxx = 0;
+
                 if (p.first != i)
                     costxx = cost[i];
 
-                if (xx > 0 && xx <= n && yy > 0 && yy <= m &&
-                    mat[xx][yy][i] > mat[p.second.first][p.second.second][i] + costxx && mat[xx][yy][i] != -1) {
-                    mat[xx][yy][i] = mat[p.second.first][p.second.second][i] + cost[i];
+                if (xx > 0 && xx <= n && yy > 0 && yy <= m && mat[xx][yy][0] != -1 &&
+                    mat[xx][yy][i] > mat[p.second.first][p.second.second][p.first] + costxx) {
+                    mat[xx][yy][i] = mat[p.second.first][p.second.second][p.first] + costxx;
                     qq.push({i, {xx, yy}});
                 }
             }
+
             for (auto &i : tunnel[p.second.first][p.second.second]) {
-                int mn=0;
-                for (int j = 0; j < 9; ++j) {
-                    if(mat[p.second.first][p.second.second][mn]>mat[p.second.first][p.second.second][j])
-                        mn=j;
+                int mn = 0;
+                for (int k = 1; k < 9; ++k) {
+                    if (mat[i.first.first][i.first.second][0] != -1 && mat[i.first.first][i.first.second][0] >
+                                                                       mat[p.second.first][p.second.second][k] +i.second
+                            ) {
+                        mat[i.first.first][i.first.second][0] = mat[p.second.first][p.second.second][k] + i.second;
+                        qq.push({0, {i.first.first, i.first.second}});
+                    }
+
                 }
-                if (mat[i.first.first][i.first.second][0] > mat[p.second.first][p.second.second][mn] + i.second &&
-                    mat[i.first.first][i.first.second][0] != -1) {
-                    mat[i.first.first][i.first.second][0] = mat[p.second.first][p.second.second][mn] + i.second;
-                    qq.push({0, {i.first.first, i.first.second}});
-                }
+
             }
 
         }
-        for (int i = 0; i < 9; ++i) {
-            cout<<mat[s2][d2][i]<<" ";
+
+        long long ans = mxxx;
+        for (int i =0; i < 9; ++i) {
+            ans = min(ans, mat[s2][d2][i]);
         }
-        cout<<endl;
-        if (mat[s2][d2][0] == mxxx)
-            mat[s2][d2][0] = -1;
+        if (ans == mxxx)
+            ans = -1;
         case(++te, ' ');
-        cout << mat[s2][d2][0] << endl;
+        cout << ans << endl;
 
 
     }
