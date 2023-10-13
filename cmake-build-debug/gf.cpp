a2a5150956ad05b96262bd54d79d9508773d416c
mehedi <mehedihasanarafat10@gmail.com>
2023-10-13 16:13:48 +0600

    building template heavy

--- cmake-build-debug/gf.cpp
+++ cmake-build-debug/gf.cpp
@@ -1,52 +1,153 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define endl '\n'
-#define inp dist6(rng)
+#define case(t, c) cout<<"Case #"<<t<<":"<<c
 
-#define lower_val 1
-#define higher_val 11
-#define min_Array_Size 1
-#define max_Array_Size 5
+
+template<typename T, typename U>
+std::istream &operator>>(std::istream &i, pair<T, U> &p) {
+    i >> p.first >> p.second;
+    return i;
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &i, vector<T> &t) {
+    for (auto &v: t) { i >> v; }
+    return i;
+}
+
+template<typename T, typename U>
+std::ostream &operator<<(std::ostream &o, const pair<T, U> &p) {
+    o << p.x << ' ' << p.y;
+    return o;
+}
+
+template<typename T>
+std::ostream &operator<<(std::ostream &o, const vector<T> &t) {
+    if (t.empty())o << '\n';
+    for (size_t i = 0; i < t.size(); ++i) { o << t[i] << " \n"[i == t.size() - 1]; }
+    return o;
+}
+
+template<typename T> using minheap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using maxheap = priority_queue<T, vector<T>, less<T>>;
+
+template<typename T>
+bool in(T a, T b, T c) { return a <= b && b < c; }
+
+namespace std {
+    template<typename T, typename U>
+    struct hash<pair<T, U>> {
+        hash<T> t;
+        hash<U> u;
+
+        size_t operator()(const pair<T, U> &p) const { return t(p.x) ^ (u(p.y) << 7); }
+    };
+}
+template<typename T>
+class vector2 : public vector<vector<T>> {
+public:
+    vector2() {}
+
+    vector2(size_t a, size_t b, T t = T()) : vector<vector<T>>(a, vector<T>(b, t)) {}
+};
+
+template<typename T>
+class vector3 : public vector<vector2<T>> {
+public:
+    vector3() {}
+
+    vector3(size_t a, size_t b, size_t c, T t = T()) : vector<vector2<T>>(a, vector2<T>(b, c, t)) {}
+};
+
+template<typename T>
+class vector4 : public vector<vector3<T>> {
+public:
+    vector4() {}
+
+    vector4(size_t a, size_t b, size_t c, size_t d, T t = T()) : vector<vector3<T>>(a, vector3<T>(b, c, d, t)) {}
+};
+
+template<typename T>
+class vector5 : public vector<vector4<T>> {
+public:
+    vector5() {}
+
+    vector5(size_t a, size_t b, size_t c, size_t d, size_t e, T t = T()) : vector<vector4<T>>(a, vector4<T>(b, c, d, e,
+                                                                                                            t)) {}
+};
+
+double ternary_search(vector<double>vc)
+{
+
+    double lo=vc[0],hi=vc.back(),cnt=300,ans=0;
+    while (cnt--)
+    {
+        double m1=lo+(hi-lo)/3,m2=hi-(hi-lo)/3;
+
+        double d1=0,d2=0;
+        for (auto &i : vc) {
+          d1=max(d1,abs(i-m1));
+          d2=max(d2,abs(i-m2));
+        }
+        if(d1>d2)
+            lo=m1;
+        else
+            hi=m2;
+        ans=m2;
+    }
+
+
+    return ans;
+}
 
 int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("here_comes_santa_claus_input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
     ios_base::sync_with_stdio(false), cin.tie(nullptr);
-    freopen("input.txt", "w", stdout);
-    auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
-    std::mt19937 rng(seed);
-    int inge = 1;
-    vector<vector<int>> vc;
-    for (int i = lower_val; i <= higher_val; ++i) {
-        for (int j = i; j <= higher_val; ++j) {
-            std::uniform_int_distribution<int> dist6(i, j);
-            for (int k = min_Array_Size; k <= max_Array_Size; ++k) {
-                // input write what type of input you want to take
 
-                vector<int> tmp;
-                for (int l = 0; l < k; ++l) {
-                    tmp.emplace_back(inp);
-                }
-                vc.emplace_back(tmp);
+    int t,te=0;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<double> vc(n);
+        cin >> vc;
+        std::sort(vc.begin(), vc.end());
+        double ans = 0;
 
-                inge += tmp.size();
+        if (n == 5) {
 
-//                if inp size >= 2.5 mb break
-
-                if (((inge * sizeof(int)) * 1e-6) > 2.5) {
-                    return 0;
-                }
-
-            }
+            ans = max(ans, ternary_search({vc[2] , vc[3] , vc[4]}) - ternary_search({vc[0] , vc[1]}));
+            ans = max(ans, ternary_search({vc[3] , vc[4]})- ternary_search({vc[0] , vc[1] , vc[2]}) );
+        } else {
+            ans = max(ans, ternary_search({vc[n-1] ,vc[n-2]})- ternary_search({vc[0] , vc[1]}));
         }
+
+        case(++te,' ');
+
+        cout << fixed << setprecision(7) << ans<< endl;
     }
 
-    cout << vc.size() << endl;
-    for (auto &k : vc) {
-        cout << k.size() << endl;
-        for (auto &i : k) {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
+
+
     return 0;
 }
+
+//Case #1: 18.0000000
+//Case #2: 2.5000000
+//Case #3: 5445.0000000
+//Case #4: 3.3333333
+//Case #5: 6.1666667
+//Case #6: 59.3333333
+//Case #7: 507.5000000
+//Case #8: 509844786.3333334
+//Case #9: 591879804.5000000
+//Case #10: 786748034.5000000
+//Case #11: 997872980.0000000
+//Case #12: 999983020.0000000
+
