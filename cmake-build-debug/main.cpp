61c70d85d56c46b081dbf4e856fd0493bd3b3e61
mehedi <mehedihasanarafat10@gmail.com>
2023-10-14 15:42:04 +0600

    template heavy-1

--- cmake-build-debug/main.cpp
+++ cmake-build-debug/main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 #define endl '\n'
-#define dtype long long
+#define dtype  long long
+#define mod 1000000007
 #define case(t, c) cout<<"Case "<<t<<":"<<c
 
 
@@ -82,11 +84,7 @@ public:
 
 int main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
 
-    }
 
     return 0;
 }
